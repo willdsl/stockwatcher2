1f910c363b79f28d1c38c2f5c035d8015b67aafe
willdsl <william.dsl@gmail.com>
2012-09-15 10:46:12 -0700

    Update src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
    commit from github

--- src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -51,7 +51,7 @@ public class StockWatcher implements EntryPoint {
   /*I am adding a new method to this class!!! */
   
   public void newMethod(){
-	  System.out.println("Git is Awesome!!");
+	  System.out.println("Git is Awesome!!!!");
   }
 
   /**
